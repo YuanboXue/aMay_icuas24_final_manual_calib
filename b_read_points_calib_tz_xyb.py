19ff3353e4792561f7645ce02f78929264eef7da
xyb <15651860528@163.com>
2024-05-06 15:37:20 +0800

    more images 0506

--- b_read_points_calib_tz_xyb.py
+++ b_read_points_calib_tz_xyb.py
@@ -57,7 +57,7 @@ images = glob.glob('/opencv_data/*.jpg')
 
 # Loop over the range
 # for i in range(1, 4):
-for i in (1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36):
+for i in (1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46):
     # Construct the file name
     # img = cv.imread(f"opencv_data/left0{i}.jpg")
     img = cv.imread(f"icuas_data/icuas_0{i}.png")
